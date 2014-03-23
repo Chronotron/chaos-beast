752feacd3eb723fa603684d6c21c8168d8a3f3b3
Chronotron <paulchronotron@gmail.com>
2014-03-23 14:00:53 -0500

    added a test class for password validator - tests to come

---
+++ Password Validator/test/password/validator/PasswordValidatorTest.java
@@ -0,0 +1,47 @@
+package password.validator;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Chrontron2
+ */
+public class PasswordValidatorTest {
+
+	public PasswordValidatorTest() {
+	}
+
+	@BeforeClass
+	public static void setUpClass() {
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+	}
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	/**
+	 * Test of main method, of class PasswordValidator.
+	 */
+	@Test
+	public void testMain() {
+		System.out.println("main");
+		String[] args = null;
+		//PasswordValidator.main(args);
+		// TODO review the generated test code and remove the default call to fail.
+		Assert.assertEquals(0, 0);
+	}
+
+}
