8e37c27c876c92e8e183041f004599a99fdc531e
Chronotron <paulchronotron@gmail.com>
2014-05-10 22:25:45 -0500

    Addition assertions for locker puzzle test

--- Locker/src/Main.java
+++ Locker/src/Main.java
@@ -1,8 +1,6 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Main {
 
--- Locker/test/MainTest.java
+++ Locker/test/MainTest.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,7 +15,7 @@ public class MainTest {
     @Before
     public void setUp() throws Exception {
         outputFile = new File("output.txt");
-        if(outputFile.exists()) {
+        if (outputFile.exists()) {
             outputFile.delete();
         }
     }
@@ -32,14 +34,36 @@ public class MainTest {
 
         // post conditions
         Assert.assertTrue(outputFile.exists());
-	Scanner scanner = new Scanner(outputFile);
-	ArrayList<String> lines = new ArrayList();
+        Scanner scanner = new Scanner(outputFile);
+        ArrayList<String> lines = new ArrayList();
 
-	while(scanner.hasNext()) {
-		lines.add(scanner.nextLine());
-	}
+        while (scanner.hasNext()) {
+            lines.add(scanner.nextLine());
+        }
 
-	Assert.assertEquals(100, lines.size());
-	Assert.assertEquals("Locker is open", lines.get(0));
+        Assert.assertEquals(100, lines.size());
+        Assert.assertEquals("Locker is open", lines.get(0)); //First
+        Assert.assertEquals("Locker is closed", lines.get(1)); /// Second
+        Assert.assertEquals("Locker is closed", lines.get(2)); // Third
+        Assert.assertEquals("Locker is open", lines.get(3)); // Fourth
+        Assert.assertEquals("Locker is closed", lines.get(4)); // Fifth
+        Assert.assertEquals("Locker is closed", lines.get(5)); // Sixth
+        Assert.assertEquals("Locker is closed", lines.get(6)); // Seventh
+        Assert.assertEquals("Locker is closed", lines.get(7)); // Eighth
+        Assert.assertEquals("Locker is open", lines.get(8)); // Ninth
+        Assert.assertEquals("Locker is closed", lines.get(9)); // Tenth
+
+        Iterator<String> itr = lines.iterator();
+        int openLinesCount = 0;
+
+        while(itr.hasNext())
+        {
+            if(itr.next().equals("Locker is open"))
+            {
+                openLinesCount++;
+            }
+        }
+
+        Assert.assertEquals(10, openLinesCount);
     }
 }
