15bb3fb06d72b82b6b0a0972abef4b767bc829b6
Chronotron <paulchronotron@gmail.com>
2016-06-15 08:19:04 -0500

    unit 2 assignment 3:w

---
+++ CS225/Unit 2/Assignment3/Assignment3/ParkerPaulAssignment3.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double weeklySalary = 200.00; // initialize base weekly salary
+	double commissionPercent = 0.09; // initialize commission percent
+	double weeklySales = 0.00; // initialize weekly sales to non -1 value
+
+	// main input loop
+	while (weeklySales != -1)
+	{
+		cout << "Enter sales in dollars (-1 to end): ";
+		cin >> weeklySales;
+		cout << "Salary is: $" << weeklySalary + (weeklySales * commissionPercent) << endl;
+	} // end while
+}
