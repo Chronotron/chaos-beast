6e3daad39bc07b1da888608752410814b3efb4ff
Chronotron <paulchronotron@gmail.com>
2016-06-24 09:27:47 -0500

    refactoring assignment 5

--- CS225/Unit 3/Assignment5/Assignment5/ParkerPaulAssignment5.cpp
+++ CS225/Unit 3/Assignment5/Assignment5/ParkerPaulAssignment5.cpp
@@ -5,25 +5,60 @@
 using namespace std;
 
 void calcWealth();
-double promptPleaseEnter(string);
+bool isRich(int, long, int, long);
+void printBanner();
+int promptPleaseEnterInt(string);
+long promptPleaseEnterLong(string);
 
 int main()
 {
-	return 0;
+	string exitSignal = "";
+	while (exitSignal != "exit")
+	{
+		calcWealth();
+		cout << "any key to continue or 'exit' to exit" << endl;
+		cin >> exitSignal;
+		cin.clear();
+	}
 }
 
 void calcWealth()
 {
-	// prompt "Please enter your age: "
-	// prompt "Please the amount of cash on hand: "
-	// prompt "Please the amount of dependents you have: "
-	// prompt "Please the amount of money you owe: "
+	int age = promptPleaseEnterInt("your age");
+	long cashOnHand = promptPleaseEnterLong("amount of cash on hand");
+	int numberOfDependents = promptPleaseEnterInt("amount of dependents you have");
+	long amountOwed = promptPleaseEnterLong("amount of money you owe");
+
+	string message = isRich(age, cashOnHand, numberOfDependents, amountOwed) ?  "Congratulations! We consider you as being \"rich\"" : "I am sorry! You are not yet \"rich\"";
+	cout << message << endl;
 }
 
-double promptPleaseEnter(string prompt)
+bool isRich(int age, long cashOnhand, int numberOfDependents, long amountOwed)
 {
-	double enteredValue = 0.0;
-	cout << ("Please enter " + prompt);
+	return 0;
+}
+
+void printBanner()
+{
+
+}
+
+int promptPleaseEnterInt(string prompt)
+{
+	int enteredValue = 0;
+
+	cout << ("Please enter " + prompt + ": ");
+	cin >> enteredValue;
+
+	return enteredValue;
+}
+
+long promptPleaseEnterLong(string prompt)
+{
+	long enteredValue = 0;
+
+	cout << ("Please enter " + prompt + ": ");
+	cin >> enteredValue;
 
 	return enteredValue;
 }
