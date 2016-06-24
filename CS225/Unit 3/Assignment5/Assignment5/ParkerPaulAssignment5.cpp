48063969a2758097a266115642e1c6fb89f98d52
Chronotron <paulchronotron@gmail.com>
2016-06-24 09:00:21 -0500

    assignment 5

---
+++ CS225/Unit 3/Assignment5/Assignment5/ParkerPaulAssignment5.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void calcWealth();
+double promptPleaseEnter(string);
+
+int main()
+{
+	return 0;
+}
+
+void calcWealth()
+{
+	// prompt "Please enter your age: "
+	// prompt "Please the amount of cash on hand: "
+	// prompt "Please the amount of dependents you have: "
+	// prompt "Please the amount of money you owe: "
+}
+
+double promptPleaseEnter(string prompt)
+{
+	double enteredValue = 0.0;
+	cout << ("Please enter " + prompt);
+
+	return enteredValue;
+}
