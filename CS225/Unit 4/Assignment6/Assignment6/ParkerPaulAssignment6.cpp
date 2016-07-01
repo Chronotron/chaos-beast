daa514fbe5b837bf5dae319793702e4fbdeb7491
Chronotron <paulchronotron@gmail.com>
2016-07-01 10:54:53 -0500

    assignment 6

--- CS225/Unit 4/Assignment6/Assignment6/ParkerPaulAssignment6.cpp
+++ CS225/Unit 4/Assignment6/Assignment6/ParkerPaulAssignment6.cpp
@@ -6,9 +6,15 @@
 
 using namespace std;
 
-vector<int [3]> generateSalesSlips(void);
+#pragma region Prototypes
+
+vector<vector<int>> generateSalesSlips();
+int getSalesFor(int, int, const vector<vector<int>>);
+void insertSales(vector<vector<int>> &, int, size_t, size_t);
 void populateSales(int[][4], int);
 void printSales(const int[][4], int);
+
+#pragma endregion
 
 int main()
 {
@@ -21,28 +27,87 @@ int main()
 	return 0;
 }
 
+#pragma region Helper Functions
+
+vector<vector<int>> generateSalesSlips()
+{
+	int count1 = rand() % 6;
+	int count2 = rand() % 6;
+	int count3 = rand() % 6;
+	int count4 = rand() % 6;
+	vector<vector<int>> salesSlips(count1 + count2 + count3 + count4);
+	insertSales(salesSlips, 1, count1, 0);
+	insertSales(salesSlips, 2, count2, count1);
+	insertSales(salesSlips, 3, count3, count1 + count2);
+	insertSales(salesSlips, 4, count4, count1 + count2 + count3);
+	return salesSlips;
+}
+
+int getSalesFor(int productId, int salesId, const vector<vector<int>> salesSlips)
+{
+	int totalSales = 0;
+
+	for (size_t i = 0; i < salesSlips.size(); i++)
+	{
+		vector<int> slip = salesSlips[i];
+		if(slip[0] == salesId && slip[1] == productId)
+		{
+			totalSales += slip[2];
+		}
+	}
+
+	return totalSales;
+}
+
+void insertSales(vector<vector<int>> &salesSlips, int id, size_t count, size_t offset)
+{
+	for (size_t i = offset; i < count + offset; i++)
+	{
+		int product = rand() % 6 + 1;
+		int sales = rand() % 100 + 1;
+		vector<int> slip(3);
+		slip[0] = id;
+		slip[1] = product;
+		slip[2] = sales;
+		salesSlips[i] = slip;
+	}
+}
+
 void populateSales(int sales[][4], int rows)
 {
-	vector<int [3]> salesSlips = generateSalesSlips();
+	vector<vector<int>> salesSlips = generateSalesSlips();
 	for (int i = 0; i < rows; i++)
 	{
+		int productId = i + 1;
 		for (int j = 0; j < 4; j++) 
 		{
-			sales[i][j] = (i + 1) * (j + 1);
+			int salesId = j + 1;
+			sales[i][j] = getSalesFor(productId, salesId, salesSlips);
 		}
 	}
 }
 
 void printSales(const int sales[][4], int rows)
 {
+	int columnTotals [4] = {0, 0, 0, 0};
 	for (int i = 0; i < rows; i++)
 	{
+		int rowTotal = 0;
 		for (int j = 0; j < 4; j++) 
 		{
 			int sale = sales[i][j];
-			printf("%*d ", 2, sale);
+			rowTotal += sale;
+			columnTotals[j] += sale;
+			printf("%*d ", 5, sale);
 		}
 
-		cout << endl;
+		cout << " " << rowTotal << endl;
 	}
+	for (int k = 0; k < 4; k++)
+	{
+		printf("%*d ", 5, columnTotals[k]);
+	}
+	cout << endl;
 }
+
+#pragma endregion
