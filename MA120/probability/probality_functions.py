bced8af66c0913f8203d59799af01c6d9f24d864
Chronotron <paulchrontron@gmail.com>
2017-07-08 18:55:58 -0500

    start of probability dists

--- MA120/probability/probality_functions.py
+++ MA120/probability/probality_functions.py
@@ -4,7 +4,6 @@ import math
 
 
 class Probability(Fraction, object):
-
     def probability(self):
         return 1 - self.relative_probability()
 
@@ -12,17 +11,25 @@ class Probability(Fraction, object):
         return self.numerator / self.denominator
 
 
+def combination(objects, selections):
+    return math.factorial(objects) / (math.factorial(objects - selections) * math.factorial(selections))
+
+
+def mean():
+    pass
+
+
 def probability(success, total):
     return Probability(success, total).probability()
+
+
+def permutation(objects, selections):
+    return math.factorial(objects) / math.factorial(objects - selections)
 
 
 def relative_probability(success, total):
     return Probability(success, total).relative_probability()
 
 
-def combination(objects, selections):
-    return math.factorial(objects) / (math.factorial(objects - selections) * math.factorial(selections))
-
-
-def permutation(objects, selections):
-    return math.factorial(objects) / math.factorial(objects - selections)
+def standard_deviation():
+    pass
