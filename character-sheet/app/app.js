eaf30ae7c35157a3c4e48dd4bc0188eb4f15c039
Chronotron <paulchrontron@gmail.com>
2017-07-17 21:35:28 -0500

    flesh out character sheet app

---
+++ character-sheet/app/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+if (!String.prototype.format) {
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] !== 'undefined' ? args[number] : match;
+        });
+    };
+}
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'characterSheet',
+    'myApp.version'
+]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
+
+    $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
---
+++ character-sheet/app/components/characterSheet/characterSheet.component.js
@@ -0,0 +1,63 @@
+(function () {
+    angular.module('characterSheet', [
+        'ngRoute'
+    ]).config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/Character/:id', {
+            template: '<character-sheet character="$resolve.character.data"></character-sheet>',
+            resolve: {
+                character: function ($http, $route) {
+                    var format = 'json/character_{0}.json'.format($route.current.params.id);
+                    return $http.get(format);
+                }
+            }
+        });
+
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }]);
+
+    angular.module('characterSheet').component('characterSheet', {
+        template: '' +
+        '<div>' +
+        '<character-info character="$ctrl.character"></character-info>' +
+        '<stat-block stats="$ctrl.character.stats"></stat-block>' +
+        '</div>',
+        bindings: {
+            character: '='
+        }
+    });
+
+    angular.module('characterSheet').component('characterInfo', {
+        template: '<label for="characterName">Name: </label><input id="characterName" type="text" ng-model="$ctrl.character.name">',
+        bindings: {
+            character: '='
+        }
+    });
+
+    angular.module('characterSheet').component('statBlock', {
+        controller: StatBlockController,
+        template: '' +
+        '<div ng-repeat="stat in $ctrl.stats">' +
+        '<label for="stat{{stat.name}}" ng-bind="stat.name"></label>' +
+        '<input id="stat{{stat.name}}" type="number" ng-model="stat.value">' +
+        '<label>Mod: {{$ctrl.calculateMod(stat.value)}}</label>' +
+        '</div>',
+        bindings: {
+            stats: '='
+        }
+    });
+
+    function StatBlockController() {
+        var $ctrl = this;
+
+        $ctrl.calculateMod = calculateMod;
+
+        function calculateMod(value) {
+            if(!value) {
+                return 0;
+            }
+
+            return Math.floor((value - 10) / 2);
+        }
+    }
+
+})();
