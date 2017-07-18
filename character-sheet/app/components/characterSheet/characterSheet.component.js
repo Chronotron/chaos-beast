489049f07cf3cce6478e062674ee2e0b591209fa
Chronotron <paulchrontron@gmail.com>
2017-07-18 07:47:41 -0500

    flesh out character sheet app - styles

--- character-sheet/app/components/characterSheet/characterSheet.component.js
+++ character-sheet/app/components/characterSheet/characterSheet.component.js
@@ -3,11 +3,10 @@
         'ngRoute'
     ]).config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/Character/:id', {
-            template: '<character-sheet character="$resolve.character.data"></character-sheet>',
+            template: '<character-sheet character="$resolve.character"></character-sheet>',
             resolve: {
-                character: function ($http, $route) {
-                    var format = 'json/character_{0}.json'.format($route.current.params.id);
-                    return $http.get(format);
+                character: function (CharacterService, $route) {
+                    return CharacterService.getCharacter($route.current.params.id);
                 }
             }
         });
@@ -15,17 +14,45 @@
         $routeProvider.otherwise({redirectTo: '/view1'});
     }]);
 
+    //region Components
+
     angular.module('characterSheet').component('characterSheet', {
+        controller: CharacterSheetController,
         template: '' +
+        '<form name="characterForm" class="character-form" ng-submit="$ctrl.saveCharacter()">' +
+        '<div class="form-inputs">' +
+        '<button type="submit">Save</button>' +
+        '<button type="button" ng-click="$ctrl.cancel()">Cancel</button>' +
+        '</div>' +
         '<div class="character-sheet">' +
         '<character-info character="$ctrl.character"></character-info>' +
         '<hr>' +
         '<stat-block stats="$ctrl.character.stats"></stat-block>' +
-        '</div>',
+        '</div>' +
+        '</form>',
         bindings: {
             character: '='
         }
     });
+
+    CharacterSheetController.$inject = ['CharacterService'];
+    function CharacterSheetController(CharacterService) {
+        var $ctrl = this;
+
+        $ctrl.cancel = cancel;
+        $ctrl.saveCharacter = saveCharacter;
+
+        function saveCharacter() {
+            CharacterService.saveCharacter($ctrl.character);
+        }
+
+        function cancel() {
+            CharacterService.getCharacter($ctrl.character.id).then(function (character) {
+                $ctrl.character = character;
+            });
+        }
+
+    }
 
     angular.module('characterSheet').component('characterInfo', {
         template: '<label for="characterName">Name: </label><input id="characterName" type="text" ng-model="$ctrl.character.name">',
@@ -39,7 +66,7 @@
         template: '' +
         '<div class="stat-block" ng-repeat="stat in $ctrl.stats">' +
         '<label class="stat-name" for="stat{{stat.name}}" ng-bind="stat.name"></label>' +
-        '<input id="stat{{stat.name}}" type="number" ng-model="stat.value">' +
+        '<input id="stat{{stat.name}}" type="number" ng-model="stat.value" min="1" max="20" required>' +
         '<label class="stat-mod">Mod: {{$ctrl.calculateMod(stat.value)}}</label>' +
         '</div>',
         bindings: {
@@ -60,5 +87,45 @@
             return Math.floor((value - 10) / 2);
         }
     }
+
+    //endregion
+
+    //region Services
+
+    angular.module('characterSheet').service('CharacterService', CharacterService);
+
+    CharacterService.$inject = ['$http', '$q'];
+    function CharacterService($http, $q) {
+        var service = this;
+
+        var characterIdMap = {};
+
+        service.getCharacter = getCharacter;
+        service.saveCharacter = saveCharacter;
+
+        function getCharacter(id) {
+            var character = angular.copy(characterIdMap[id]);
+            if(character) {
+                var deferred = $q.defer();
+                deferred.resolve(character);
+                return deferred.promise;
+            }
+            var url = 'json/character_{0}.json'.format(id);
+            return $http.get(url).then(function (response) {
+                character = response.data;
+                if(character) {
+                    characterIdMap[character.id] = character;
+                }
+                return angular.copy(character);
+            });
+        }
+
+        function saveCharacter(character) {
+            characterIdMap[character.id] = angular.copy(character);
+        }
+
+    }
+
+    //endregion
 
 })();
