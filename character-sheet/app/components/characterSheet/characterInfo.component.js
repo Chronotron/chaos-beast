164cd98d2ec50c7c869ed91b3ab26576151963c8
Chronotron <paulchrontron@gmail.com>
2017-07-20 16:49:07 -0500

    max length on character name

--- character-sheet/app/components/characterSheet/characterInfo.component.js
+++ character-sheet/app/components/characterSheet/characterInfo.component.js
@@ -1,6 +1,6 @@
 (function () {
     angular.module('characterSheet').component('characterInfo', {
-        template: '<label for="characterName">Name: </label><input id="characterName" ng-model="$ctrl.character.name">',
+        template: '<label for="characterName">Name: </label><input id="characterName" ng-model="$ctrl.character.name" ng-maxlength="200">',
         bindings: {
             character: '='
         }
